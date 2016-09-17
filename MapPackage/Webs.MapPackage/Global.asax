c28974ba178f539ed5ad39ae5ee26b3bf3e459d1
Nguyen Quoc <langtuvotinh773@yahoo.com>
2016-09-17 23:12:30 +0700

    All
    All

---
+++ MapPackage/Webs.MapPackage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webs.MapPackage.MvcApplication" Language="C#" %>
---
+++ MapPackage/Webs.MapPackage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webs.MapPackage.Startup))]
+namespace Webs.MapPackage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
